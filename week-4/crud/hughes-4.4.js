fec8bc1c9d44068ae97303558babb62b09e235ff
Joshua Hughes <joshie373@gmail.com>
2019-08-28 22:24:36 -0700

    adding 4.4

---
+++ week-4/crud/hughes-4.4.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title:  hughes-4.4.js
+; Author: Professor Krasso
+; Date:   28 August 2019
+; Description: Demonstrates CRUD operations in a
+;              Node.js API.
+; Modified by: Joshua Hughes
+;===========================================
+*/
+
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.get("/", function(request, response) {
+  response.send("API invoked as an HTTP GET request.");
+});
+
+app.put("/", function(request, response) {
+  response.send("API invoked as an HTTP PUT request.");
+});
+
+app.post("/", function(request, response) {
+  response.send("API invoked as an HTTP POST request");
+});
+
+app.delete("/", function(request, response) {
+  response.send("API invoked as an HTTP DELETE request");
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
