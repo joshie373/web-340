d6c6585f5783fe1faf84907d6778eb6003c236f2
Joshua Hughes <joshie373@gmail.com>
2019-10-03 20:12:37 -0700

    adding heroku docs

--- ems/app.js
+++ ems/app.js
@@ -201,6 +201,7 @@ db.once("open",function(){
 
 
 //starts application on port 8080
-http.createServer(app).listen(8080, function() {
-    console.log("Application started on port 8080!");
+app.set("port", process.env.PORT || 8080);
+http.createServer(app).listen(app.get("port"), function() { 
+    console.log("Application started on port " + app.get("port") + "!") 
 });
